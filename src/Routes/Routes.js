17ff168497e9279d16144b88fb01a3cc4f4bace5
RubensSantos00 <rubenscvsantos@gmail.com>
2021-09-21 23:38:08 -0300

    Router

---
+++ src/Routes/Routes.js
@@ -0,0 +1,13 @@
+import history from 'react-router-dom'
+
+export const goToHome = (history) => {
+    history.push('/')
+}
+
+export const goToPokedex = (history) => {
+    history.push('/pokedex')
+}
+
+export const goToDetails = (history) => {
+     history.push('/details')
+}
